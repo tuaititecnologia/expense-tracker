0dbf0dcffe7ff43fcf3f392ae6bfe86ebe459753
Alejandro Sanchez <sanchezalejandroismael@gmail.com>
2023-10-13 11:44:11 -0300

    Fix create admin first

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,14 +15,14 @@ class DatabaseSeeder extends Seeder
 
         $this->call(CategorySeeder::class);
 
-
-        \App\Models\User::factory(4)->create();
-
         \App\Models\User::factory()->create([
             'name' => 'Alejandro Sanchez',
             'email' => 'alejandro.sanchez@tuaiti.com.ar',
             'password' => '$2y$10$x2Uffur9fdtfysinqtzjwuK.Xki2THkpjGqeUL9OrJHrN5P.MEWxG',
         ]);
+
+        \App\Models\User::factory(4)->create();
+
 
         $this->call(ExpenseSeeder::class);
     }
