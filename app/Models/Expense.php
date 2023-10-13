cc6f0091e491ed6e8595999a622efd59266fbd20
Alejandro Sanchez <sanchezalejandroismael@gmail.com>
2023-10-13 09:28:07 -0300

    Add relationships

--- app/Models/Expense.php
+++ app/Models/Expense.php
@@ -5,10 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Expense extends Model
 {
     use HasFactory;
 
     public $guarded = [];
+
+    public function category(): BelongsTo {
+        return $this->belongsTo(Category::class);
+    }
 }
