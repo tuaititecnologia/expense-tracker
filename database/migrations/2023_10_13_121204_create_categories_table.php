aefe0aba513547228069e080626db623ba979f40
Alejandro Sanchez <sanchezalejandroismael@gmail.com>
2023-10-13 09:15:04 -0300

    Delete unused code

--- database/migrations/2023_10_13_121204_create_categories_table.php
+++ database/migrations/2023_10_13_121204_create_categories_table.php
@@ -18,11 +18,4 @@ return new class extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('categories');
-    }
 };
